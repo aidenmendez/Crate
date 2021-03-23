2918833cb6c16af71d63a04f3a58481039b1129a
Vanessa Arruda <wanessarruda@gmail.com>
2021-03-23 10:10:53 -0600

    add be comments to schema index.js

--- code/api/src/setup/schema/index.js
+++ code/api/src/setup/schema/index.js
@@ -1,14 +1,23 @@
+// Creates the schema
+
 // Imports
 import { GraphQLSchema } from 'graphql'
+
+// Imports the query using relative path syntax
+// 'query' variable is set in the queries.js file
+// Same for mutation
 
 // App Imports
 import query from './queries'
 import mutation from './mutations'
 
+// Sets constant 'schema' using variables from both files above
 // Schema
 const schema = new GraphQLSchema({
   query,
   mutation
 })
 
+// Makes this available for other files to import
+// Maybe similar to environment variables?
 export default schema
