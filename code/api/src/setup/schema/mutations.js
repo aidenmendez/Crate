2b45041e693f411301f1e53c46c43512546fb60f
Vanessa Arruda <wanessarruda@gmail.com>
2021-03-23 10:10:53 -0600

    add be comments to schema mutation.js

--- code/api/src/setup/schema/mutations.js
+++ code/api/src/setup/schema/mutations.js
@@ -1,12 +1,16 @@
 // Imports
 import { GraphQLObjectType } from 'graphql'
 
+// Imports all mutations (which I think act like migrations) for user, product, subscription and crate models
+// Looks like SQL, i.e import all from table user from the mutation file for this model
+
 // App Imports
 import * as user from '../../modules/user/mutations'
 import * as product from '../../modules/product/mutations'
 import * as crate from '../../modules/crate/mutations'
 import * as subscription from '../../modules/subscription/mutations'
 
+// Defines 'mutation' constant as a GraphQL Object
 // Mutation
 const mutation = new GraphQLObjectType({
   name: 'mutations',
@@ -20,4 +24,5 @@ const mutation = new GraphQLObjectType({
   }
 })
 
+// Gives access to schema
 export default mutation
