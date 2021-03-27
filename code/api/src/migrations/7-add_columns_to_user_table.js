1beffb21a6a58a462890e0cea48275d88c7feee4
Aiden Mendez <aidenmendez@gmail.com>
2021-03-26 18:27:55 -0600

    Fix data type and naming conventions

--- code/api/src/migrations/7-add_columns_to_user_table.js
+++ code/api/src/migrations/7-add_columns_to_user_table.js
@@ -6,10 +6,10 @@ module.exports = {
       queryInterface.addColumn('users', 'description', {
         type: Sequelize.STRING
       }),
-      queryInterface.addColumn('users', 'address_line_1', {
+      queryInterface.addColumn('users', 'addressLine1', {
         type: Sequelize.STRING
       }),
-      queryInterface.addColumn('users', 'address_line_2', {
+      queryInterface.addColumn('users', 'addressLine2', {
         type: Sequelize.STRING
       }), 
       queryInterface.addColumn('users', 'city', {
@@ -19,22 +19,22 @@ module.exports = {
         type: Sequelize.STRING
       }),
       queryInterface.addColumn('users', 'zipcode', {
-        type: Sequelize.STRING
+        type: Sequelize.INTEGER
       }),
       queryInterface.addColumn('users', 'image', {
         type: Sequelize.STRING
       })
-    ]  
+    ] 
   },
 
   down: async (queryInterface, Sequelize) => {
-    queryInterface.removeColumn('Users', 'description'),
-    queryInterface.removeColumn('Users', 'address_line_1'),
-    queryInterface.removeColumn('Users', 'address_line_2'),
-    queryInterface.removeColumn('Users', 'city'),
-    queryInterface.removeColumn('Users', 'state'),
-    queryInterface.removeColumn('Users', 'zipcode'),
-    queryInterface.removeColumn('Users', 'image')
+    queryInterface.removeColumn('users', 'description'),
+    queryInterface.removeColumn('users', 'address_line_1'),
+    queryInterface.removeColumn('users', 'address_line_2'),
+    queryInterface.removeColumn('users', 'city'),
+    queryInterface.removeColumn('users', 'state'),
+    queryInterface.removeColumn('users', 'zipcode'),
+    queryInterface.removeColumn('users', 'image')
   }
 };
  // description, address_line_1, address_line_2, city, state, zipcode, image
