3dbf0eea119982c07d1f94105e7a73382d06cc43
Aiden Mendez <aidenmendez@gmail.com>
2021-03-26 18:45:59 -0600

    Implement try/ catch blocks in a user table migration

--- code/api/src/migrations/7-add_columns_to_user_table.js
+++ code/api/src/migrations/7-add_columns_to_user_table.js
@@ -2,39 +2,46 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return [
-      queryInterface.addColumn('users', 'description', {
+    try {
+      await queryInterface.addColumn('users', 'description', {
         type: Sequelize.STRING
-      }),
-      queryInterface.addColumn('users', 'addressLine1', {
+      });
+      await queryInterface.addColumn('users', 'addressLine1', {
         type: Sequelize.STRING
-      }),
-      queryInterface.addColumn('users', 'addressLine2', {
+      });
+      await queryInterface.addColumn('users', 'addressLine2', {
         type: Sequelize.STRING
-      }), 
-      queryInterface.addColumn('users', 'city', {
+      });
+      await queryInterface.addColumn('users', 'city', {
         type: Sequelize.STRING
-      }),
-      queryInterface.addColumn('users', 'state', {
+      });
+      await queryInterface.addColumn('users', 'state', {
         type: Sequelize.STRING
-      }),
-      queryInterface.addColumn('users', 'zipcode', {
+      });
+      await queryInterface.addColumn('users', 'zipcode', {
         type: Sequelize.INTEGER
-      }),
-      queryInterface.addColumn('users', 'image', {
+      });
+      await queryInterface.addColumn('users', 'image', {
         type: Sequelize.STRING
-      })
-    ] 
+      });
+      return Promise.resolve();
+    } catch (e) {
+      return Promise.reject(e)
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    queryInterface.removeColumn('users', 'description'),
-    queryInterface.removeColumn('users', 'address_line_1'),
-    queryInterface.removeColumn('users', 'address_line_2'),
-    queryInterface.removeColumn('users', 'city'),
-    queryInterface.removeColumn('users', 'state'),
-    queryInterface.removeColumn('users', 'zipcode'),
-    queryInterface.removeColumn('users', 'image')
+    try {
+      await queryInterface.removeColumn('users', 'description');
+      await queryInterface.removeColumn('users', 'address_line_1');
+      await queryInterface.removeColumn('users', 'address_line_2');
+      await queryInterface.removeColumn('users', 'city');
+      await queryInterface.removeColumn('users', 'state');
+      await queryInterface.removeColumn('users', 'zipcode');
+      await queryInterface.removeColumn('users', 'image');
+      return Promise.resolve();
+    } catch (e) {
+      return Promise.reject(e);
+    }
   }
 };
- // description, address_line_1, address_line_2, city, state, zipcode, image
