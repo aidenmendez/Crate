7f4acaa20bf8fde2a347a918eec6647f770f0b5f
Alia Peterson <alia@pnwebs.com>
2021-03-29 11:07:23 -0600

    Add user profile details and styling

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -9,11 +9,15 @@ import { Link } from 'react-router-dom'
 import { Grid, GridCell } from '../../ui/grid'
 import { H3, H4 } from '../../ui/typography'
 import Button from '../../ui/button'
+import ImageTile from '../../ui/image/Tile'
+import { level3 } from '../../ui/common/shadows'
 import { grey, grey2 } from '../../ui/common/colors'
 
 // App Imports
 import userRoutes from '../../setup/routes/user'
 import { logout } from './api/actions'
+
+const userImage = `https://media.newyorker.com/photos/5e49bf473399bf0008132231/1:1/w_2539,h_2539,c_limit/Kenseth-CatProfile.jpg`
 
 // Component
 const Profile = (props) => (
@@ -30,17 +34,47 @@ const Profile = (props) => (
       </GridCell>
     </Grid>
 
-    <Grid>
-      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+    {/* User information */}
+    <Grid justifyCenter={true} alignCenter={true}>
+      <GridCell style={{
+          marginLeft: '2em',
+          padding: '2em',
+          textAlign: 'center',
+          objectFit: 'contain',
+          maxWidth: 250,
+          maxHeight: 250
+        }}>
+        <ImageTile height={250} width={250} image={userImage} shadow={level3}/>
+      </GridCell>
+
+      <GridCell>
         <H4 style={{ marginBottom: '0.5em' }}>{props.user.details.name}</H4>
 
-        <p style={{ color: grey2, marginBottom: '2em' }}>{props.user.details.email}</p>
+        <p style={{ color: grey2, marginBottom: '2em' }}>User Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio</p>
+        <p style={{ color: grey2, marginBottom: '2em' }}>Email Address: {props.user.details.email}</p>
+        <p style={{ color: grey2, marginBottom: '2em' }}>Shipping Address: 123 Main Street, Denver, CO 80602</p>
+
+
+      </GridCell>
+    </Grid>
+
+    {/* Buttons */}
+    <Grid>
+      <GridCell style={{ flex: '' , textAlign: 'center', marginBottom: '2em' }}>
+        <Button theme="primary" onClick={() => console.log('wow!')} style={{ marginRight: '1em' }}>Update Profile</Button>
 
         <Link to={userRoutes.subscriptions.path}>
           <Button theme="primary">Subscriptions</Button>
         </Link>
 
         <Button theme="secondary" onClick={props.logout} style={{ marginLeft: '1em' }}>Logout</Button>
+      </GridCell>
+    </Grid>
+
+    {/* Bottom title bar */}
+    <Grid style={{ backgroundColor: grey }}>
+      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+        <H3 font="secondary">My orders</H3>
       </GridCell>
     </Grid>
   </div>
