868e5cf7966626c24e150ae2ffb89d20882a08a9
Elsa Fluss <elsafluss@gmail.com>
2021-04-01 11:44:27 -0600

    # This is a combination of 2 commits. # This is the 1st commit message:
    Add success message on profile save.
    
    # This is the commit message #2:
    
    Reformat update profile

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -8,21 +8,23 @@ import { Link } from 'react-router-dom'
 // UI Imports
 import { Grid, GridCell } from '../../ui/grid'
 import { H3, H4 } from '../../ui/typography'
-import Button from '../../ui/button'
-import ImageTile from '../../ui/image/Tile'
-import Card from '../../ui/card/Card'
-import Input from '../../ui/input/Input'
-import Textarea from '../../ui/input/Textarea'
 import { level3 } from '../../ui/common/shadows'
 import { white, black, grey, grey2 } from '../../ui/common/colors'
 
 // App Imports
 import { APP_URL } from '../../setup/config/env'
 import userRoutes from '../../setup/routes/user'
-import { logout } from './api/actions'
+import { logout } from "./api/actions"
 import { getListByUser } from '../subscription/api/actions'
 import { updateUser, toggleForm } from '../user/api/actions'
-import { routeImage } from '../../setup/routes'
+import { messageShow, messageHide } from "../common/api/actions"
+import { routeImage } from "../../setup/routes"
+import Button from '../../ui/button'
+import Card from '../../ui/card/Card'
+import EmptyMessage from "../common/EmptyMessage"
+import ImageTile from '../../ui/image/Tile'
+import Input from '../../ui/input/Input'
+import Textarea from '../../ui/input/Textarea'
 
 const mockProductHistory = [
   {
@@ -75,6 +77,10 @@ class Profile extends PureComponent {
   // Submit controlled form data
   handleSubmit = () => {
     this.props.toggleForm()
+    this.props.messageShow('Profile updated successfully.')
+    window.setTimeout(() => {
+      this.props.messageHide()
+    }, 5000)
   }
 
   // Runs on client only
@@ -144,7 +150,7 @@ class Profile extends PureComponent {
               >
                 <Button
                   theme="secondary"
-                  onClick={() => console.log("Update date")}
+                  onClick={() => console.log("Update date", sub.createdAt)}
                   type="button"
                 >
                   Update Shipping Date
@@ -165,135 +171,182 @@ class Profile extends PureComponent {
 
         {/* Top title bar */}
         <Grid style={{ backgroundColor: grey }}>
-          <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+          <GridCell style={{ padding: "2em", textAlign: "center" }}>
             <H3 font="secondary">My profile</H3>
           </GridCell>
         </Grid>
 
         {/* User information */}
         <Grid justifyCenter={true} alignCenter={true}>
-          <GridCell style={{
-              marginLeft: '2em',
-              padding: '2em',
-              textAlign: 'center',
+          <GridCell
+            style={{
+              marginLeft: "2em",
+              padding: "2em",
+              textAlign: "center",
               maxWidth: 250,
               maxHeight: 250,
-            }}>
+            }}
+          >
             <ImageTile
-              style={{backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'}}
+              style={{
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
               height={250}
               width={250}
-              image={this.state.currentUser.image || ''}
-              shadow={level3}/>
+              image={this.state.currentUser.image || ""}
+              shadow={level3}
+            />
           </GridCell>
 
-          {this.props.user.showForm ?
+          {this.props.user.showForm ? (
             <GridCell>
-              <H4 style={{ marginBottom: '0.5em' }}>{this.props.user.details.name}</H4>
+              <H4 style={{ marginBottom: "0.5em", padding: "1rem" }}>
+                {this.props.user.details.name}
+              </H4>
+              <Grid style={{ margin: '1rem'}}>
+                <GridCell>
+                  <label htmlFor="user-description">User Description:</label>
+                  <br />
+                  <Textarea
+                    id="user-description"
+                    name="description"
+                    onChange={this.handleChange}
+                    value={this.state.currentUser.description || ""}
+                  ></Textarea>
+                  <br />
 
-              <label htmlFor='user-description'>User Description:</label><br/>
-              <Textarea
-                id='user-description'
-                name='description'
-                onChange={this.handleChange}
-                value={this.state.currentUser.description || ''}>
-              </Textarea><br/>
+                  <label htmlFor="user-email">User Email:</label>
+                  <Input
+                    id="user-email"
+                    name="email"
+                    onChange={this.handleChange}
+                    value={this.state.currentUser.email || ""}
+                    type="text"
+                  ></Input>
+                  <br />
 
-              <label htmlFor='user-email'>User Email:</label>
-              <Input
-                id='user-email'
-                name='email'
-                onChange={this.handleChange}
-                value={this.state.currentUser.email || ''}
-                type='text'>
-              </Input><br/>
+                  <label htmlFor="user-address1">User Address Line 1:</label>
+                  <Input
+                    id="user-address1"
+                    name="addressLine1"
+                    onChange={this.handleChange}
+                    value={this.state.currentUser.addressLine1 || ""}
+                    type="text"
+                  ></Input>
+                  <br />
 
-            <label htmlFor='user-address1'>User Address Line 1:</label>
-              <Input
-                id='user-address1'
-                name='addressLine1'
-                onChange={this.handleChange}
-                value={this.state.currentUser.addressLine1 || ''}
-                type='text'>
-              </Input><br/>
+                  <label htmlFor="user-address2">User Address Line 2:</label>
+                  <Input
+                    id="user-address2"
+                    name="addressLine2"
+                    onChange={this.handleChange}
+                    value={this.state.currentUser.addressLine2 || ""}
+                    type="text"
+                  ></Input>
+                </GridCell>
+                <GridCell>
+                  <label htmlFor="user-city">City:</label>
+                  <Input
+                    id="user-city"
+                    name="city"
+                    onChange={this.handleChange}
+                    value={this.state.currentUser.city || ""}
+                    type="text"
+                  ></Input>
 
-            <label htmlFor='user-address2'>User Address Line 2:</label>
-              <Input
-                id='user-address2'
-                name='addressLine2'
-                onChange={this.handleChange}
-                value={this.state.currentUser.addressLine2 || ''}
-                type='text'>
-              </Input>
+                  <label htmlFor="user-state">State:</label>
+                  <Input
+                    id="user-state"
+                    name="state"
+                    onChange={this.handleChange}
+                    value={this.state.currentUser.state || ""}
+                    type="text"
+                  ></Input>
 
-            <label htmlFor='user-city'>City:</label>
-              <Input
-                id='user-city'
-                name='city'
-                onChange={this.handleChange}
-                value={this.state.currentUser.city || ''}
-                type='text'>
-              </Input>
+                  <label htmlFor="user-zipcode">Zip Code:</label>
+                  <Input
+                    id="user-zipcode"
+                    name="zipcode"
+                    onChange={this.handleChange}
+                    value={this.state.currentUser.zipcode || ""}
+                    type="text"
+                  ></Input>
 
-            <label htmlFor='user-state'>State:</label>
-              <Input
-                id='user-state'
-                name='state'
-                onChange={this.handleChange}
-                value={this.state.currentUser.state || ''}
-                type='text'>
-              </Input>
-
-            <label htmlFor='user-zipcode'>Zip Code:</label>
-              <Input
-                id='user-zipcode'
-                name='zipcode'
-                onChange={this.handleChange}
-                value={this.state.currentUser.zipcode || ''}
-                type='text'>
-              </Input>
-
-            <label htmlFor='user-image'>Image:</label>
-              <Input
-                id='user-image'
-                name='image'
-                onChange={this.handleChange}
-                value={this.state.currentUser.image || ''}
-                type='text'>
-              </Input>
-
-            </GridCell> :
-
-            <GridCell>
-              <H4 style={{ marginBottom: '0.5em' }}>{this.state.currentUser.name}</H4>
-
-              <p style={{ color: grey2, marginBottom: '2em' }}>User Description: {this.state.currentUser.description}</p>
-              <p style={{ color: grey2, marginBottom: '2em' }}>Email Address: {this.state.currentUser.email}</p>
-              <p style={{ color: grey2, marginBottom: '2em' }}>Shipping Address: {this.state.currentUser.addressLine1} {this.state.currentUser.addressLine2}, {this.state.currentUser.city}, {this.state.currentUser.state} {this.state.currentUser.zipcode}</p>
-
+                  <label htmlFor="user-image">Image:</label>
+                  <Input
+                    id="user-image"
+                    name="image"
+                    onChange={this.handleChange}
+                    value={this.state.currentUser.image || ""}
+                    type="text"
+                  ></Input>
+                </GridCell>
+              </Grid>
             </GridCell>
-          }
+          ) : (
+            <GridCell>
+              <H4 style={{ marginBottom: "0.5em" }}>
+                {this.state.currentUser.name}
+              </H4>
+
+              <p style={{ color: grey2, marginBottom: "2em" }}>
+                User Description: {this.state.currentUser.description}
+              </p>
+              <p style={{ color: grey2, marginBottom: "2em" }}>
+                Email Address: {this.state.currentUser.email}
+              </p>
+              <p style={{ color: grey2, marginBottom: "2em" }}>
+                Shipping Address: {this.state.currentUser.addressLine1}{" "}
+                {this.state.currentUser.addressLine2},{" "}
+                {this.state.currentUser.city}, {this.state.currentUser.state}{" "}
+                {this.state.currentUser.zipcode}
+              </p>
+            </GridCell>
+          )}
         </Grid>
 
         {/* Buttons */}
         <Grid>
-          <GridCell style={{ flex: '' , textAlign: 'center', marginBottom: '2em' }}>
-            {this.props.showForm ?
-              <Button theme="secondary" onClick={this.handleSubmit} style={{ marginRight: '1em' }}>Save Profile</Button> :
-              <Button theme="secondary" onClick={this.props.toggleForm} style={{ marginRight: '1em' }}>Update Profile</Button>
-            }
+          <GridCell
+            style={{ flex: "", textAlign: "center", marginBottom: "2em" }}
+          >
+            {this.props.showForm ? (
+              <Button
+                theme="secondary"
+                onClick={this.handleSubmit}
+                style={{ marginRight: "1em" }}
+              >
+                Save Profile
+              </Button>
+            ) : (
+              <Button
+                theme="secondary"
+                onClick={this.props.toggleForm}
+                style={{ marginRight: "1em" }}
+              >
+                Update Profile
+              </Button>
+            )}
 
             <Link to={userRoutes.subscriptions.path}>
               <Button theme="primary">Subscriptions</Button>
             </Link>
 
-            <Button theme="secondary" onClick={this.props.logout} style={{ marginLeft: '1em' }}>Logout</Button>
+            <Button
+              theme="secondary"
+              onClick={this.props.logout}
+              style={{ marginLeft: "1em" }}
+            >
+              Logout
+            </Button>
           </GridCell>
         </Grid>
 
         {/* Bottom title bar */}
         <Grid style={{ backgroundColor: grey }}>
-          <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+          <GridCell style={{ padding: "2em", textAlign: "center" }}>
             <H3 font="secondary">My orders</H3>
           </GridCell>
         </Grid>
@@ -302,22 +355,26 @@ class Profile extends PureComponent {
         <Grid>
           <GridCell>
             {/* Card for each subscription crate - map through each in list array */}
-            <H4 font='primary' style={{ margin: '2rem' }}>Current Orders</H4>
-              {this.props.subscriptions &&
-                <Grid>
-                  {subs}
-                </Grid>
-              }
+            <H4 font="primary" style={{ margin: "2rem" }}>
+              Current Orders
+            </H4>
+            {this.props.subscriptions.length !== 0 ? (
+              <Grid>{subs}</Grid>
+            ) : (
+              <Grid>
+                <EmptyMessage message="You are not subscribed to any crates yet." />
+              </Grid>
+            )}
           </GridCell>
         </Grid>
 
         {/* Past Orders */}
         <Grid>
           <GridCell>
-            <H4 font='primary' style={{ margin: '2rem' }}>Past Orders</H4>
-            <Grid>
-              {products}
-            </Grid>
+            <H4 font="primary" style={{ margin: "2rem" }}>
+              Past Orders
+            </H4>
+            <Grid>{products}</Grid>
           </GridCell>
         </Grid>
       </div>
@@ -329,7 +386,10 @@ class Profile extends PureComponent {
 Profile.propTypes = {
   user: PropTypes.object.isRequired,
   logout: PropTypes.func.isRequired,
-  subscriptions: PropTypes.array
+  subscriptions: PropTypes.array,
+  showForm: PropTypes.bool,
+  messageShow: PropTypes.func.isRequired,
+  messageHide: PropTypes.func.isRequired
 }
 
 // Component State
@@ -341,4 +401,4 @@ function profileState(state) {
   }
 }
 
-export default connect(profileState, { getListByUser, toggleForm, logout })(Profile)
+export default connect(profileState, { getListByUser, toggleForm, logout, messageShow, messageHide })(Profile)
