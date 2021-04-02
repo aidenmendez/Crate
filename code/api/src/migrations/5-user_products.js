db9024f26f5cf9dda877fba92fbd3e91983ca9e0
Aiden Mendez <aidenmendez@gmail.com>
2021-04-01 20:46:15 -0600

    Add createdAt and updatedAt attributes to userProducts

--- code/api/src/migrations/5-user_products.js
+++ code/api/src/migrations/5-user_products.js
@@ -28,6 +28,14 @@ module.exports = {
       kept: {
         type: Sequelize.BOOLEAN,
         allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
       }
     })
 
--- code/api/src/seeders/4-userProducts.js
+++ code/api/src/seeders/4-userProducts.js
@@ -9,31 +9,41 @@ module.exports = {
         id: 1,
         userId: 2,
         productId: 2,
-        kept: false
+        kept: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         id: 2,
         userId: 2,
         productId: 4,
-        kept: false
+        kept: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         id: 3,
         userId: 2,
         productId: 7,
-        kept: true
+        kept: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         id: 4,
         userId: 2,
         productId: 8,
-        kept: false
+        kept: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         id: 5,
         userId: 2,
         productId: 3,
-        kept: true
+        kept: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ])
   }
